98b49948e26de369584254d56b4dd8df172e505d
rohitbhamu <rohitbhamu6@gmail.com>
2019-01-17 00:51:54 +0530

    job done without using handler,Loopers and handler threads

--- app/src/main/java/com/example/rohitkumarbhamu/looperhandlerhandler_threadexample/MainActivity.java
+++ app/src/main/java/com/example/rohitkumarbhamu/looperhandlerhandler_threadexample/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.rohitkumarbhamu.looperhandlerhandler_threadexample;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
     private SimpleWorker worker;
     private TextView tvMessage;
 
+    private Handler handler= new Handler( Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            tvMessage.setText((String)msg.obj);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         worker = new SimpleWorker();
+        tvMessage = (TextView)findViewById(R.id.tv_message);
 
         worker.execute(() -> {
             try {
@@ -24,11 +36,38 @@ public class MainActivity extends AppCompatActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            Message message = Message.obtain();
+            message.obj= "Task 1 completed";
+            handler.sendMessage(message);
         }).execute(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Message message = Message.obtain();
+            message.obj= "Task 2 completed";
+            handler.sendMessage(message);
 
         }).execute(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Message message = Message.obtain();
+            message.obj= "Task 3 completed";
+            handler.sendMessage(message);
 
         });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        worker.quit();
     }
 }
